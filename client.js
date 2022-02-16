b62a137bd4cca7e577631ba557df531724e1725c
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-16 15:54:39 -0700

    added movement and corrected an error

--- client.js
+++ client.js
@@ -1,1 +1,35 @@
-const { connect } = require('./play');
+const net = require("net");
+
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  conn.on("connect", (Name) => {
+    console.log("connection successful!");
+    conn.write("Name: AMS");
+    //conn.write("Move: up");
+  }); 
+
+  return conn;
+};
+
+
+module.exports = {
+  connect
+}
+
+
+
+
+
+console.log("Connecting ...");
+connect();

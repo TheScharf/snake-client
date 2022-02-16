36f2a46bbe001d44ddec04b40fbf60091f83b626
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-16 16:23:23 -0700

    added a file for client inputs

---
+++ input.js
@@ -0,0 +1,23 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function() {
+    process.stdin.on('data', (key) => {
+      process.stdout.write('clrt' + 'c');
+    });
+  };
+
+  stdin.on('data', handleUserInput);
+
+  setupInput();
+
+  return stdin;
+};
+
+
+module.exports = {
+  setupInput
+}

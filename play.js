9d4082531f015851ae951c0d8829c8b717049414
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-16 16:22:21 -0700

    made changes

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
+console.log("Connecting ...");
+connect();
 
-  const handleUserInput = function() {
-    process.stdin.on('data', (key) => {
-      process.stdout.write('clrt' + 'c');
-    });
-  };
-
-  stdin.on('data', handleUserInput);
-
-  setupInput();
-
-  return stdin;
-};
-
+setupInput;

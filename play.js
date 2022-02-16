6d62ed64192aa4b3d46c5555814e091dd55b16d6
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-16 16:13:38 -0700

    added a quit feature

--- play.js
+++ play.js
@@ -1,1 +1,21 @@
 const { connect } = require('./client');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function() {
+    process.stdin.on('data', (key) => {
+      process.stdout.write('clrt' + 'c');
+    });
+  };
+
+  stdin.on('data', handleUserInput);
+
+  setupInput();
+
+  return stdin;
+};
+

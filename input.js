7f291fe3d1d823cf541124222f6a7595067989f8
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-16 18:39:33 -0700

    added movement

--- input.js
+++ input.js
@@ -1,21 +1,37 @@
-const setupInput = function() {
+let connection;
+
+
+const setupInput = function(conn) {
+
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  const handleUserInput = function() {
-    process.stdin.on('data', (key) => {
-      process.stdout.write('clrt' + 'c');
-    });
-  };
-
-  stdin.on('data', handleUserInput);
-
-  setupInput();
-
   return stdin;
 };
+
+const handleUserInput = process.stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+
+});
+
 
 
 module.exports = {
